d32f9b1ef372c064ef60a7dec64d5fa5988b9052
AlexVoytenko <54215817+AlexVoytenko@users.noreply.github.com>
2020-06-28 20:45:56 +0300

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,25 @@
+﻿namespace B20_Ex02_Itay_311446876_Alex_322038399
+{ 
+    internal class Cell<T>
+    {
+        private T m_ObjInCell;
+        private bool m_IsExposed = false;
+
+        internal Cell(T i_ObjToSet)
+        {
+            m_ObjInCell = i_ObjToSet;
+        }
+
+        internal T ObjInCell
+        {
+            get { return m_ObjInCell; }
+            set { m_ObjInCell = value; }
+        }
+
+        internal bool IsExposed
+        {
+            get { return m_IsExposed; }
+            set { m_IsExposed = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace B20_Ex02_Itay_311446876_Alex_322038399
+{
+    public class Program
+    {
+        // Main function of the game that starts the game
+        public static void Main()
+        {
+            MemoryGame game = new MemoryGame();
+            game.StartGame();
+        }
+    }
+}
